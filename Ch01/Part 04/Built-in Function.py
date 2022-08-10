eeab65d6139509ee79633423311a69d20f1e41b1
Yoon-kiyeong <kyyoon2105@naver.com>
2022-08-10 14:10:08 +0900

    내장 함수
    내장 함수 ~ zip

--- Ch01/Part 04/Built-in Function.py
+++ Ch01/Part 04/Built-in Function.py
@@ -128,3 +128,153 @@ b = 3
 print(isinstance(b,Person))
 #b는 Person 클래스가 만든 인스턴스가 아니므로 False를 돌려준다.
 
+#2022.08.10
+#len
+#len(s)은 입력값 s의 길이(요소의 전체 개수)를 돌려주는 함수이다.
+print(len("python"))
+print(len([1,2,3]))
+print(len((1,'a')))
+
+#list
+#list(s)는 반복 가능한 자료형 s를 입력받아 리스트로 만들어 돌려주는 함수이다.
+print(list("python"))
+print(list((1,2,3)))
+#list 함수에 리스트를 입력으로 주면 똑같은 리스트를 복사하여 돌려준다.
+a = [1,2,3]
+b  = list(a)
+print(b)
+
+#map
+#map(f,iterable)은 함수(f)와 반복 가능한(iterable) 자료형을 입력으로 받는다. map은 입력 받은 자료형의 각 요소를 함수 f가 수행한 결과를 묶어서 돌려주는 함수이다.
+def two_times(numberList):
+    result = []
+    for number in numberList:
+        result.append(number*2)
+    return result
+
+result = two_times([1,2,3,4,])
+print(result)
+#two_times 함수는 리스트 요소를 입력받아 각 요소에 2를 곱한 결괏값을 돌려준다.
+
+#위 예제는 map 함수를 사용하면 다음처럼 바꿀 수 있다.
+def two_times(x): 
+    return x * 2
+print(list(map(two_times,[1,2,3,4])))
+#앞 예제를 해석해 보자. 먼저 리스트의 첫 번째 요소인 1이 two_times 함수의 입력값으로 들어가고 1 * 2의 과정을 거쳐서 2가 된다.
+#다음으로 리스트의 두 번째 요소인 2가 2 * 2의 과정을 거쳐 4가 된다. 따라서 결괏값 리스트는 이제 [2,4]가 된다.
+#총 4개의 요솟값이 모두 수행되면 마지막으로 [2,4,6,8]을 돌려준다. 이것이 map 함수가 하는 일이다.
+
+#앞의 예는 lambda를 사용하면 다음처럼 간략하게 만들 수 있다.
+print(list(map(lambda a:a * 2, [1,2,3,4])))
+
+#max
+#max(iterable)은 인수로 반복 가능한 자료형을 입력받아 그 최댓값을 돌려주는 함수이다.
+print(max([1,2,3]))
+print(max("python"))
+
+#min
+#min(iterable)은 max 함수와 반대로, 인수로 반복 가능한 자료형을 입력받아 그 최솟값을 돌려주는 함수이다.
+print(min([1,2,3]))
+print(min("python"))
+
+#oct
+#oct(x)는 정수 형태의 숫자를 8진수 문자열로 바꾸어 돌려주는 함수이다.
+print(oct(34))
+print(oct(12345))
+
+#open
+#open(filename,[mode])은 '파일 이름'과 '읽기 방법'을 입력받아 파일 객체를 돌려주는 함수이다.
+#읽기 방법(mode)을 생략하면 기본값인 읽기 전용 모드(r)로 파일 객체를 만들어 돌려준다.
+#   모드            설명
+#   w           쓰기 모드로 파일 열기
+#   r           읽기 모드로 파일 열기
+#   a           추가 모드로 파일 열기
+#   b           바이너리 모드로 파일 열기
+#b는 w,r,a와 함께 사용한다.
+# f = open("binary_file","rb")
+#위 예제의 rb는 '바이너리 읽기 모드'를 의미한다.
+
+#다음 예의 fread와 fread2는 동일한 방법이다.
+# fread = open("read_mode.txt",'r')
+# fread2 = open("read_mode.txt")
+#즉 모드 부분을 생략하면 기본값으로 읽기 모드 r를 갖게 된다.
+
+#다음은 추가 모드(a)로 파일을 여는 예이다.
+# fappend = open("apeend_mode.txt",'a')
+
+#ord
+#ord(c)는 문자의 아스키 코드 값을 돌려주즌 함수이다.
+#ord 함수는 chr 함수와 반대이다.
+print(ord('a'))
+print(ord('0'))
+
+#pow
+#pow(x,y)는 x의 y 제곱한 결괏값을 돌려주는 함수이다.
+print(pow(2,4))
+print(pow(3,3))
+
+#range
+#range([start,]stop[,stop])는 for문과 함께 자주 사용하는 함수이다. 이 함수는 입력받은 숫자에 해당하는 범위 값을 반복 가능한 객체로 만들어 돌려준다.
+
+#인수가 하나인 경우
+#시작 숫자를 지정해 주지 않으면 range 함수는 0부터 시작한다.
+print(list(range(5)))
+
+#인수가 2개인 경우
+#입력으로 주어지는 2개의 인수는 시작 숫자와 끝 숫자를 나타낸다. 단 끝 숫자는 해당 범위에 포함되지 않는다는 것에 주의하자.
+print(list(range(5,10)))#끝 숫자 10은 포함하지 않음
+
+#인수가 3개인 경우
+#세 번째 인수는 숫자 사이의 거리를 말한다.
+print(list(range(1,10,2)))  #1부터 9까지, 숫자 사이의 거리는 2
+print(list(range(0,-10,-1)))    #0부터 -9까지, 숫자 사이의 거리는 -1
+
+#round
+#round(number[,ndigits])함수는 숫자를 입력받아 반올림해 주는 함수이다.
+#[,ndigits]는 ndigits가 있을 수도 있고 없을수도 있다는 의미이다.
+print(round(4.6))
+print(round(4.2))
+
+#다음과 같이 실수 5.678을 소수점 2자리까지만 반올림하여 표시할 수 있다.
+print(round(5.678,2))
+#round 함수의 두 번째 매개변수는 반올림하여 표시하고 싶은 소수점의 자릴수(ndigits)이다.
+
+#sorted
+#sorted(iterable) 함수는 입력값을 정렬한 후 그 결과를 리스트로 돌려주는 함수이다.
+print(sorted([3,1,2]))
+print(sorted(['a','c','b']))
+print(sorted("zero"))
+print(sorted((3,2,1)))
+#리스트 자료형에도 sort 함수가 있다. 하지만 리스트 자료형의 sort 함수는 리스트 객체 그 자체를 정렬만 할 뿐 정렬된 결과를 돌려주지는 않는다.
+
+#str
+#str(object)은 문자열 형태로 객체를 변환하여 돌려주는 함수이다.
+print(str(3))
+print(str('hi'))
+print(str('hi'.upper()))
+
+#sum
+#sum(iterable)은 입력받은 리스트나 튜플의 모든 요소의 합을 돌려주는 함수이다.
+print(sum([1,2,3]))
+print(sum((4,5,6)))
+
+#tuple
+#tuple(iterable)은 반복 가능한 자료형을 입력받아 튜플 형태로 바꾸어 돌려주는 함수이다.
+#만약 튜플이 입력으로 들어오면 그대로 돌려준다.
+print(tuple("abc"))
+print(tuple([1,2,3]))
+print(tuple((1,2,3)))
+
+#type
+#type(object)은 입력값의 자료형이 무엇인지 알려 주는 함수이다.
+print(type("abc"))
+print(type([]))
+print(type(()))
+print(type(open("test",'w')))
+
+#zip
+#zip(*iterable)은 동일한 개수로 이루어진 자료형을 묶어 주는 역할을 하는 함수이다.
+#여기서 사용한 *iterable은 반복 가능(iter-able)한 자료형 여러 개를 입력할 수 있다는 의미이다.
+print(list(zip([1,2,3,],[4,5,6])))
+print(list(zip([1,2,3,],[4,5,6,],[7,8,9])))
+print(list(zip("abc","def")))
